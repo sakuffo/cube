2e2c61a0669a964028801ea0faf7c2fd8abe9de0
Stephen Akuffo <sa.akuffo@gmail.com>
2024-06-03 14:17:24 +0000

    stats

---
+++ worker/stats.go
@@ -0,0 +1,20 @@
+package worker
+
+import (
+	"github.com/c9s/goprocinfo"
+)
+
+type Stats struct {
+	MemStats *linux.MemInfo
+	DiskStats *linux.Disk
+	CpuStats *linux.CPUStat
+	LoadStats *linux.LoadAvg
+}
+
+func (s *Stats) MemTotalKb() uint64 {
+	return s.MemStats.MemTotal
+}
+
+func (s *Stats) MemAvailableKb() uint64 {
+	return s.MemStats.MemAvailable
+}

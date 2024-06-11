737f9eb945629c9b05039185973a562af254fa27
Stephen Akuffo <sa.akuffo@gmail.com>
2024-06-11 03:31:34 +0000

    manager basic code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cube/manager"
 	"cube/task"
 	"cube/worker"
 	"fmt"
@@ -28,7 +29,48 @@ func main() {
 
 	go runTasks(&w)
 	go w.CollectStats()
-	api.Start()
+	go api.Start()
+
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(workers)
+
+	for i := 0; i < 3; i++ {
+		uuidString := uuid.New()
+		t := task.Task{
+			ID:    uuidString,
+			Name:  fmt.Sprintf(("test-container-%d-%s"), i, uuidString.String()[:3]),
+			State: task.Scheduled,
+			Image: "strm/helloworld-http",
+		}
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+		m.AddTask(te)
+
+		// Original code was just the m.SendWork() call. But it itermittently failed to send the work to the worker
+		// So I added the for loop to keep trying until the work is sent and the sleep to give the worker time to process the task
+		for m.Pending.Len() != 0 {
+			m.SendWork()
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	go func() {
+		for {
+			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+			m.UpdateTasks()
+			time.Sleep(15 * time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDb {
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+			time.Sleep(15 * time.Second)
+		}
+	}
 }
 
 func runTasks(w *worker.Worker) {

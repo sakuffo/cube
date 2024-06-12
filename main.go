a3d08b5b30037a7e275f8d05afecdbeaaf3126cf
Stephen Akuffo <sa.akuffo@gmail.com>
2024-06-12 18:15:25 +0000

    cleaned up prototype code

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -44,28 +43,5 @@ func main() {
 	go m.UpdateTasks()
 
 	mapi.Start()
-
-	for i := 0; i < 3; i++ {
-		uuidString := uuid.New()
-		t := task.Task{
-			ID:    uuidString,
-			Name:  fmt.Sprintf(("test-container-%d-%s"), i, uuidString.String()[:3]),
-			State: task.Scheduled,
-			Image: "strm/helloworld-http",
-		}
-		te := task.TaskEvent{
-			ID:    uuid.New(),
-			State: task.Running,
-			Task:  t,
-		}
-		m.AddTask(te)
-
-		// Original code was just the m.SendWork() call. But it itermittently failed to send the work to the worker
-		// So I added the for loop to keep trying until the work is sent and the sleep to give the worker time to process the task
-		for m.Pending.Len() != 0 {
-			m.SendWork()
-			time.Sleep(1 * time.Second)
-		}
-	}
 
 }
